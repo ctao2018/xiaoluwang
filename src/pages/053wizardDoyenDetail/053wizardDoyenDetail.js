7c57c569fab5c6c0432ba3880eef55aacc4d8243
arno <764479956@qq.com>
2018-07-26 18:29:48 +0800

    054-056

--- src/pages/053wizardDoyenDetail/053wizardDoyenDetail.js
+++ src/pages/053wizardDoyenDetail/053wizardDoyenDetail.js
@@ -1,9 +1,12 @@
 const content = require('./053wizardDoyenDetail.ejs');
 const botSource = require('../../plugin/053wizardDoyenDetail.ejs');
 const layout = require('../../template/layout.js');
+const scrollWall = require('../../template/scrollWall.ejs');
 const tcm = require('../../template/title-count-more.ejs');
 const comment = require('../../template/comment-item.ejs');
 const guideRecord = require('../../template/guide-record-item.ejs');
+const itemWithPic = require('../../template/item-with-pic.ejs');
+const dialogForGuide = require('../../template/dialog-guide-service.ejs');
 const pf = {
   pageTitle: '向导服务达人详情',
   showTitle: false,
@@ -21,5 +24,13 @@ module.exports = layout.init({
 }).run(content({
   tcm,
   comment,
-  guideRecord
+  guideRecord,
+  itemWithPic,
+  scrollWall: scrollWall({
+    w: 750,
+    h: 450
+  }),
+  dialogForGuide: dialogForGuide({
+    itemWithPic: itemWithPic
+  })
 }), '', botSource());
